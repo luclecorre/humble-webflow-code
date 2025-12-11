fa20d94d95ed93caa7df49b4ffd2a7f22bef8ab4
luclecorre <luc@studiohumble.co.uk>
2025-12-11 10:29:32 +0000

    Preloader Fix v1.1

--- js/gsap-animations.js
+++ js/gsap-animations.js
@@ -30,8 +30,8 @@ function initLogoRevealLoader(){
   .to(progressBar, { scaleX: 1 })
   .to(logo, { clipPath:"inset(0% 0% 0% 0%)" }, "<")
   .to(container,{ autoAlpha: 0, duration: 0.5 })
-  .to(progressBar,{ scaleX: 0, transformOrigin: "right center", duration: 0.5},"<")
-  .add("hideContent", "<")
+  .set(progressBar, { autoAlpha: 0 })
+  .add("hideContent")
   .to(bg, { yPercent: -101, duration: 1 },"hideContent")
   .set(wrap,{ display: "none" });
   
